7af69fc535928a5dd86b604861caf419893e11f7
fadelrizky11 <fadelrizky11@gmail.com>
2022-11-24 11:27:56 +0700

    first commit

---
+++ lib/screens/profile.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('assets/bayiki.jpeg'),
+              ),
+              Text(
+                'Fadel Rizky',
+                style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Computer Engineering",
+                style: TextStyle(
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(Icons.phone, color: Colors.teal),
+                    title: Text(
+                      "082226108484",
+                      style: TextStyle(
+                          color: Colors.teal.shade900, fontSize: 20.0),
+                    ),
+                  )),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "fadelrizky11@gmail.com",
+                    style:
+                        TextStyle(fontSize: 20.0, color: Colors.teal.shade900),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.home,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "Semarang",
+                    style:
+                        TextStyle(fontSize: 20.0, color: Colors.teal.shade900),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
